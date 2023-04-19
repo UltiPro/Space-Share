eb8f69768db8fdd914109ac414ae2d4be70e72d7
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-04-19 19:30:43 +0200

    Add newsletter url

--- SpaceShare/Blog/urls.py
+++ SpaceShare/Blog/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
          views.AuthorPostsByTag.as_view(), name="author_posts_by_tag"),
     path("about-us", views.About.as_view(), name="about_us"),
     path("register", views.Register.as_view(), name="register"),
-    path("login", views.Login.as_view(), name="login")
+    path("login", views.Login.as_view(), name="login"),
+    path("newsletter", views.Newsletter.as_view(), name="newsletter")
 ]
