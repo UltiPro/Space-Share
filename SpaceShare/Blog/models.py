648d6b5fd843df3c2a62c87312fe6b3a87effb9a
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-04-25 16:48:48 +0200

    Add default user image and migrations

--- SpaceShare/Blog/models.py
+++ SpaceShare/Blog/models.py
@@ -43,7 +43,8 @@ class User(models.Model):
         "^[a-zA-Z]\w*$", message="Incorrect expression of nickname.")])
     email = models.EmailField(unique=True, validators=[RegexValidator(
         "^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$", message="Incorrect expression of e-mail.")])
-    image = models.ImageField(upload_to="users", null=True)
+    image = models.ImageField(
+        upload_to="users", null=True, default="users/default.png")
 
     def __str__(self):
         return self.nickname
