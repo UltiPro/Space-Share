0cb8600a33979f0d21d98446dba26ccb1b16953d
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-04-29 02:28:35 +0200

    Author slugify and little correction

--- SpaceShare/Blog/models.py
+++ SpaceShare/Blog/models.py
@@ -18,13 +18,17 @@ class Author(models.Model):
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=50)
     description = models.TextField(
-        validators=[MinLengthValidator(30), MaxLengthValidator(2000)], default="Information about author")
+        validators=[MinLengthValidator(15), MaxLengthValidator(2000)], default="Information about author")
     email = models.EmailField(validators=[RegexValidator(
         "^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$", message="Incorrect expression of e-mail.")])
     image = models.ImageField(upload_to="authors", null=True)
 
     def __str__(self):
         return f'{self.name} {self.surname}'
+
+    def save(self, *args, **kwargs):
+        self.slug = "-".join((slugify(self.name), slugify(self.surname)))
+        super().save(*args, **kwargs)
 
 
 class Tag(models.Model):
