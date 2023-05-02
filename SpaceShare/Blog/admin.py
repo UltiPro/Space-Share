12483681b1c653da30a62df0f83e536f1df1c045
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-05-02 02:42:44 +0200

    Improve admin panel

--- SpaceShare/Blog/admin.py
+++ SpaceShare/Blog/admin.py
@@ -1,35 +1,62 @@
 from django.contrib import admin
+from django.http.request import HttpRequest
+from .models import Newsletter, Author, Tag, Post, User, Comment
 
-from .models import Newsletter, Author, Tag, User, Comment, Post
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "email")
+    search_fields = ("name", "surname")
+    readonly_fields = ("name", "surname", "email")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request: HttpRequest, obj=...):
+        return False
 
 
 class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "email")
+    search_fields = ("name", "surname")
     prepopulated_fields = {
         "slug": ("name", "surname")
     }
 
 
 class UserAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "email")
+    search_fields = ("nickname", "email")
+    readonly_fields = ("login", "password", "email")
     prepopulated_fields = {
         "slug": ("nickname",)
     }
 
+    def has_add_permission(self, request):
+        return False
+
 
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("user", "post", "date")
+    list_filter = ("date", )
+    search_fields = ("user", "post")
+    readonly_fields = ("post", "user")
+
+    def has_add_permission(self, request):
+        return False
 
 
 class PostAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "date")
+    list_filter = ("author", "tags", "date")
+    search_fields = ("author", "title")
     prepopulated_fields = {
         "slug": ("title",)
     }
-    list_filter = ("author", "tags", "date")
-    list_display = ("title", "author", "date")
 
 
-admin.site.register(Newsletter)
+admin.site.register(Newsletter, NewsletterAdmin)
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Tag)
 admin.site.register(User, UserAdmin)
-admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Post, PostAdmin)
