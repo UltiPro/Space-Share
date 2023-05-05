18b73d0f9c6a0d50748ef2507ca3f9e009245f9f
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-05-05 17:51:40 +0200

    Correct forms

--- SpaceShare/Blog/forms.py
+++ SpaceShare/Blog/forms.py
@@ -1,4 +1,3 @@
-from typing import Any
 from django import forms
 from django.core.validators import RegexValidator
 from django.contrib.auth.hashers import make_password
@@ -137,7 +136,7 @@ class ChangeEmailForm(forms.Form):
     old_email = forms.EmailField(label="New E-mail", validators=[RegexValidator(
         "^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$", message="Incorrect expression of e-mail.")], widget=forms.EmailInput({
             'class': 'form-control border border-secondary mb-2',
-            'placeholder': 'New E-mail'
+            'placeholder': 'Old E-mail'
         }))
     new_email = forms.EmailField(label="New E-mail", validators=[RegexValidator(
         "^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$", message="Incorrect expression of e-mail.")], widget=forms.EmailInput({
@@ -163,15 +162,7 @@ class ChangePasswordForm(forms.Form):
 
 class ChangeImageForm(forms.Form):
     image = forms.ImageField(widget=forms.FileInput(attrs={
-        'class': 'form-control form-control-lg',
-    }))
-
-
-class ChangeDescriptionForm(forms.Form):
-    textarea = forms.CharField(widget=forms.Textarea(attrs={
-        'rows': '8',
-        'class': "form-control form-control-sm",
-        'placeholder': "Your profile description..."
+        'class': 'form-control form-control',
     }))
 
 
@@ -181,3 +172,11 @@ class DeleteAccountForm(forms.Form):
             'class': 'form-control border border-secondary mb-2',
             'placeholder': 'Your password'
         }))
+
+
+class ChangeDescriptionForm(forms.Form):
+    textarea = forms.CharField(widget=forms.Textarea(attrs={
+        'rows': '8',
+        'class': "form-control form-control-sm",
+        'placeholder': "Your profile description..."
+    }))
