4bd34f09e9f75955d870d7815fe628dc62fd066e
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2024-02-28 15:26:19 +0100

    Update

--- SpaceShare/Blog/urls.py
+++ SpaceShare/Blog/urls.py
@@ -10,14 +10,12 @@ urlpatterns = [
     path("post/<slug:slug>", views.Post.as_view(), name="post"),
     path("authors", views.Authors.as_view(), name="authors"),
     path("author/<slug:slug>", views.Author.as_view(), name="author"),
-    path("author/<slug:slug>/posts",
-         views.AuthorPosts.as_view(), name="author_posts"),
-    path("author/<slug:slug>/posts/<str:str>",
-         views.AuthorPostsByTag.as_view(), name="author_posts_by_tag"),
+    path("author/<slug:slug>/posts", views.AuthorPosts.as_view(), name="author_posts"),
+    path("author/<slug:slug>/posts/<str:str>", views.AuthorPostsByTag.as_view(), name="author_posts_by_tag"),
     path("about-us", views.About.as_view(), name="about_us"),
     path("register", views.Register.as_view(), name="register"),
     path("login", views.Login.as_view(), name="login"),
     path("logout", views.Logout, name="logout"),
     path("settings", views.Settings.as_view(), name="settings"),
-    path("user/<slug:slug>", views.User.as_view(), name="user")
+    path("user/<slug:slug>", views.User.as_view(), name="user"),
 ]
